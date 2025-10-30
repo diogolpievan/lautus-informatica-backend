56bdb3a2a463fcf7ef0c5bca66a87c31dc9c336a
Diego Pievan <diego.pievan123@gmail.com>
2025-10-30 16:21:02 -0300

    Procedure desbloquear usuario

--- LautusInformatica/Migrations/20251030150539_CreateUserProcedures.cs
+++ LautusInformatica/Migrations/20251030150539_CreateUserProcedures.cs
@@ -10,7 +10,20 @@ namespace LautusInformatica.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"CREATE PROCEDURE sp_DesbloquearUsuario(
+                                    IN pUserId INT
+                                )
+                                BEGIN
+                                    IF EXISTS (SELECT 1 FROM users WHERE Id = pUserId) THEN
+                                        UPDATE users
+                                        SET Lockout = 0,
+                                            AccessFailedCount = 0
+                                        WHERE Id = pUserId;
+                                    ELSE
+                                        SIGNAL SQLSTATE '45000'
+                                            SET MESSAGE_TEXT = 'Not found';
+                                    END IF;
+                                END");
         }
 
         /// <inheritdoc />
