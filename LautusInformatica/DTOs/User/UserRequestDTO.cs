04ab8b991a9ab75ea0451b026a99da4e1396f419
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-01 12:16:48 -0300

    feat: Uer DTOS

--- LautusInformatica/DTOs/User/UserRequestDTO.cs
+++ LautusInformatica/DTOs/User/UserRequestDTO.cs
@@ -2,5 +2,11 @@
 {
     public class UserRequestDTO
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Address { get; set; }
     }
 }
