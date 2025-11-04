db7eaa056b5f158990d4f992fb52d46edf529aa7
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-04 08:21:50 -0300

    feat: invalid Login Exception

---
+++ LautusInformatica/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,9 @@
+﻿namespace LautusInformatica.Exceptions
+{
+    public class InvalidLoginException : AppException
+    {
+        public InvalidLoginException() 
+            : base(401, "Credenciais de login inválidas. Por favor, verifique seu email e senha.") 
+        { }
+    }
+}
