01c91c1da95e46914e1dbf5cb62b118de3d07ef0
Diego Pievan <diego.pievan123@gmail.com>
2025-11-05 16:22:55 -0300

    fix: hash

--- LautusInformatica/Interfaces/Services/IUserService.cs
+++ LautusInformatica/Interfaces/Services/IUserService.cs
@@ -12,6 +12,6 @@ namespace LautusInformatica.Interfaces.Services
         Task<bool> DeleteUser(int id);
         Task<bool> ChangePassword(int id, string newPassword);
         Task<bool> UnlockUser(int id);
-        Task<bool> UserLoginIsValid(string email, string hashedPassword);
+        Task<bool> UserLoginIsValid(string email, string password);
     }
 }
