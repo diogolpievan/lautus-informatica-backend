8aa4ab6b04e576b026f5eeb5dc44ada0b2ec9253
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-06 15:59:25 -0300

    Item DTOs

---
+++ LautusInformatica/DTOs/Item/ItemResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace LautusInformatica.DTOs.Item
+{
+    public class ItemResponseDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public Models.Enums.ItemCategory Category { get; set; }
+    }
+}
