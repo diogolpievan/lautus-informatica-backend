916e8f7b22486d4fed7982d324251f5489376fb7
Diego Pievan <diego.pievan123@gmail.com>
2025-11-06 21:41:29 -0300

    update: authId

--- LautusInformatica/Interfaces/Repositories/IUserRepository.cs
+++ LautusInformatica/Interfaces/Repositories/IUserRepository.cs
@@ -7,11 +7,11 @@ namespace LautusInformatica.Interfaces.Repositories
         Task<User> GetUserById(int id);
         Task<User> GetUserByEmail(string email);
         Task<IEnumerable<User>> GetAllUsers();
-        Task<int> CreateUser(User user);
-        Task<bool> UpdateUser(User user);
-        Task<bool> DeleteUser(int id);
-        Task<bool> ChangePassword(int id, string newPasswordHash);
-        Task<bool> UnlockUser(int id);
+        Task<int> CreateUser(User user, int authId);
+        Task<bool> UpdateUser(User user, int authId);
+        Task<bool> DeleteUser(int id, int authId);
+        Task<bool> ChangePassword(int id, string newPasswordHash, int authId);
+        Task<bool> UnlockUser(int id, int authId);
         Task<bool> UserLoginIsValid(string email, string passwordHash);
 
     }
