f2f29ff16d86b8da0752b84390aff4b935ea5deb
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-08 15:07:52 -0300

    feat: service order repository

---
+++ LautusInformatica/Interfaces/Repositories/IServiceOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using LautusInformatica.Models;
+using LautusInformatica.Models.Enums;
+
+namespace LautusInformatica.Interfaces.Repositories
+{
+    public interface IServiceOrderRepository
+    {
+        public Task<IEnumerable<ServiceOrder>?> GetAllServiceOrders();
+        public Task<IEnumerable<ServiceOrder>?> GetServiceOrdersByStatus(Status status);
+        public Task<ServiceOrder> GetServiceOrderById(int id);
+        public Task<IEnumerable<ServiceOrder>?> GetServiceOrdersByClientId(int clientId);
+        public Task<int> CreateServiceOrder(ServiceOrder serviceOrder, int authId);
+        public Task<bool> UpdateServiceOrder(ServiceOrder serviceOrder, int authId);
+        public Task<bool> DeleteServiceOrder(int id, int authId);
+        public Task<bool> ChangeServiceOrderStatus(int id, int statusId, int authId);
+        public Task<bool> CompleteServiceOrder(int id, DateOnly completionDate, int authId);
+    }
+}
