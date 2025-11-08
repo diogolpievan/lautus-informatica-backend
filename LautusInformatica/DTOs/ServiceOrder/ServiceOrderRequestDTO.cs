6bbfce711016e81097c2ef12cc88f3f261c0beba
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-08 15:05:56 -0300

    feat: service order DTOs

---
+++ LautusInformatica/DTOs/ServiceOrder/ServiceOrderRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace LautusInformatica.DTOs.ServiceOrder
+{
+    public class ServiceOrderRequestDTO
+    {
+        public string Equipment { get; set; }
+        public string Problem { get; set; }
+        public int UserId { get; set; }
+        public string? Description { get; set; }
+        public decimal ServicePrice { get; set; } = 0;
+        public DateOnly EntryDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public DateOnly? CompletionDate { get; set; }
+
+    }
+}
