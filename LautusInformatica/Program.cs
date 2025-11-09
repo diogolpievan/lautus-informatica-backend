1baf089cb4a6bc62a4f4960b442650b4c8ddf7ff
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-09 20:57:53 -0300

    feat: builder addScopped ServiceOrder

--- LautusInformatica/Program.cs
+++ LautusInformatica/Program.cs
@@ -44,10 +44,12 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
+builder.Services.AddScoped<IServiceOrderRepository, ServiceOrderRepository>();
 
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IItemService, ItemService>();
+builder.Services.AddScoped<IServiceOrderService, ServiceOrderService>();
 
 builder.Services.AddAuthentication(options =>
 {
