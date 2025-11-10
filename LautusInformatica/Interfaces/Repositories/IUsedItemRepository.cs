1aa9fd30f6d06123f28bd453acf56bf752be406d
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-10 10:39:36 -0300

    feat: interface repo used items

---
+++ LautusInformatica/Interfaces/Repositories/IUsedItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using LautusInformatica.DTOs.ServiceOrder;
+using LautusInformatica.Models;
+
+namespace LautusInformatica.Interfaces.Repositories
+{
+    public interface IUsedItemsRepository
+    {
+        Task<UsedItems?> GetUsedItemById(int id);
+        Task<IEnumerable<UsedItems>> GetUsedItemsByServiceOrder(int serviceOrderId);
+        Task<IEnumerable<UsedItems>> GetUsedItemsByItem(int itemId);
+        Task<int> CreateUsedItem(UsedItems usedItem, int authId);
+        Task<bool> UpdateUsedItem(UsedItems usedItem, int authId);
+        Task<bool> DeleteUsedItem(int id, int authId);
+    }
+}
