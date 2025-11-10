27a53fe5fa749bbe664cb509e78ee50a715e7df2
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-10 10:40:18 -0300

    feat: used items exceptions

---
+++ LautusInformatica/Exceptions/BadRequest/InvalidQuantityException.cs
@@ -0,0 +1,9 @@
+﻿namespace LautusInformatica.Exceptions.BadRequest
+{
+    public class InvalidQuantityException : BadRequestException
+    {
+        public InvalidQuantityException() 
+            : base("A quantidade informada é inválida.") 
+        { }
+    }
+}
---
+++ LautusInformatica/Exceptions/BadRequest/UsedItemNotInServiceOrderException.cs
@@ -0,0 +1,10 @@
+﻿namespace LautusInformatica.Exceptions.BadRequest
+{
+    public class UsedItemNotInServiceOrderException : BadRequestException
+    {
+        public UsedItemNotInServiceOrderException(int usedItemId, int serviceOrderId)
+            : base($"Item utilizado {usedItemId} não pertence à ordem de serviço {serviceOrderId}")
+        {
+        }
+    }
+}
