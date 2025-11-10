69d5f3a91c4bc83e8d9fcd721445073890e27ea8
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-10 10:41:15 -0300

    chore: add scoped used items

--- LautusInformatica/Program.cs
+++ LautusInformatica/Program.cs
@@ -73,11 +73,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IItemRepository, ItemRepository>();
 builder.Services.AddScoped<IServiceOrderRepository, ServiceOrderRepository>();
+builder.Services.AddScoped<IUsedItemsRepository, UsedItemsRepository>();
 
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IItemService, ItemService>();
 builder.Services.AddScoped<IServiceOrderService, ServiceOrderService>();
+builder.Services.AddScoped<IUsedItemsService, UsedItemsService>();
 
 builder.Services.AddAuthentication(options =>
 {
