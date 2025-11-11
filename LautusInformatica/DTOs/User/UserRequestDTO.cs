08d311a97fcdd7467eee4460e5dfee3201785668
Diogo Louvo Pievan <diogo.pievan@gmail.com>
2025-11-10 21:13:02 -0300

    update: password request is optionally

--- LautusInformatica/DTOs/User/UserRequestDTO.cs
+++ LautusInformatica/DTOs/User/UserRequestDTO.cs
@@ -5,7 +5,7 @@ namespace LautusInformatica.DTOs.User
     public class UserRequestDTO
     {
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public UserRole Role { get; set; }
