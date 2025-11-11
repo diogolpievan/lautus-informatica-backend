1d9d15ac4e8201936f546d3231028d90abb5b5fa
GabrielRot <xcross341@gmail.com>
2025-11-10 22:28:54 -0300

    feat: add SuperAdmin migration

---
+++ LautusInformatica/Migrations/20251110232529_SuperAdminUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LautusInformatica.Migrations
+{
+    /// <inheritdoc />
+    public partial class SuperAdminUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"call sp_CreateUser('Admin', '123456', '9999999999', 'admin@lautus.com',
+                                    0, 'Rua 123', 0, @a);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
